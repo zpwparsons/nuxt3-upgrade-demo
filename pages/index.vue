0aae11357014d873dbaa8c8ef6256e7b4c687b08
zpwparsons <peterp@zero2one.co.za>
2023-06-08 18:01:07 +0200

    chore: first commit

---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  Hello Nuxt3
+</template>
---
+++ pages/login/index.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+definePageMeta({
+    layout: 'blank',
+});
+
+const logIn = () => {
+    console.log('submitting...')
+};
+</script>
+
+<template>
+    <form @submit.prevent="logIn">
+        <div>
+            <label for="email">Email Address</label>
+            <input type="email" id="email" name="email">
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password">
+        </div>
+
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</template>
